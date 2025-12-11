dd72852467e72e1db276044b4307eee36b6e0acd
渡辺陸斗 <pi09158ku@gmail.com>
2025-12-11 22:06:02 +0900

    Apply clean .gitignore

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Unifree",
+  description: "Campus flea market app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
