cc4fb03ec9a7db8fcb3f81c47ae206723c94f03a
渡辺陸斗 <pi09158ku@gmail.com>
2025-12-14 19:58:25 +0900

    Last Error

--- src/libs/supabase/client.ts
+++ src/libs/supabase/client.ts
@@ -1,2 +1,7 @@
-export { getSupabaseBrowserClient, supabase } from '@/lib/supabase/client';
-export type { Database } from './types';
+import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "@/supabase/types";
+
+export const supabase = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
